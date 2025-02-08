ea9f5a0c855b75ba22bc6bc20177bc1383f59813
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2025-02-08 18:46:43 +0000

    single product component

---
+++ src/Components/Product/ProductCard.jsx
@@ -0,0 +1,30 @@
+import {} from "react";
+import Rating from "@mui/material/Rating";
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
+import classes from "./product.module.css";
+function ProductCard({ product }) {
+  const { image, title, id, rating, price } = product;
+  return (
+    <div className={`${classes.card__container}`}>
+      <a href="">
+        <img src={image} alt="" />
+      </a>
+      <div>
+        <h3>{title}</h3>
+        <div className={classes.rating}>
+          {/* rating */}
+          <Rating value={rating.rate} precision={0.5} />
+          {/* rating counter */}
+          <small>{rating.count}</small>
+        </div>
+        <div>
+          {/* price */}
+          <CurrencyFormat amount={price} />
+        </div>
+        <button className={classes.button}>Add to Cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
